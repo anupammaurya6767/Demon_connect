95646b19b76e4b70990779a224b4e10c3363c0b8
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-02 20:33:16 +0530

    Update send_message.py

--- features/send/send_message.py
+++ features/send/send_message.py
@@ -1,56 +1,23 @@
-# send_message.py
+# features/send/send_message.py
 
 from selenium.webdriver.common.keys import Keys
-from utils.helpers import find_element_with_retry
 
-def send_text_message(driver, contact_name, message):
-    """
-    Send a text message to a contact or group on WhatsApp Web.
+def send_message(driver, contact_name, message):
+    try:
+        # Locate the input box
+        input_box = driver.find_element_by_xpath(
+            '//*[@id="main"]/footer/div/div/span[2]/div/div[2]/div/div/div'
+        )
 
-    :param driver: WebDriver instance
-    :param contact_name: Name of the contact or group
-    :param message: The text message to send
-    :return: True if the message is sent successfully, False otherwise
-    """
-    # Find and click on the contact or group
-    contact_element = find_element_with_retry(driver, f"//span[@title='{contact_name}']")
-    if contact_element:
-        contact_element.click()
+        # Clear the input box and send the message
+        input_box.clear()
+        input_box.send_keys(message)
+        input_box.send_keys(Keys.ENTER)
 
-        # Find the message input box and send the message
-        message_box = find_element_with_retry(driver, "//div[@class='_2S1VP copyable-text selectable-text']")
-        if message_box:
-            message_box.click()
-            message_box.send_keys(message)
-            message_box.send_keys(Keys.RETURN)
-            return True
+        print(f"Message sent to {contact_name}: {message}")
 
-    return False
-# send_message.py
+    except Exception as e:
+        print(f"Failed to send a message to {contact_name}: {str(e)}")
 
-from selenium.webdriver.common.keys import Keys
-from utils.helpers import find_element_with_retry
-
-def send_text_message(driver, contact_name, message):
-    """
-    Send a text message to a contact or group on WhatsApp Web.
-
-    :param driver: WebDriver instance
-    :param contact_name: Name of the contact or group
-    :param message: The text message to send
-    :return: True if the message is sent successfully, False otherwise
-    """
-    # Find and click on the contact or group
-    contact_element = find_element_with_retry(driver, f"//span[@title='{contact_name}']")
-    if contact_element:
-        contact_element.click()
-
-        # Find the message input box and send the message
-        message_box = find_element_with_retry(driver, "//div[@class='_2S1VP copyable-text selectable-text']")
-        if message_box:
-            message_box.click()
-            message_box.send_keys(message)
-            message_box.send_keys(Keys.RETURN)
-            return True
-
-    return False
+# Usage Example:
+# send_message(driver, "Contact Name", "Hello, this is a test message!")
