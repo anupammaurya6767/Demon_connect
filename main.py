a903dcaaf727a7391c8b6ba78f423bf8e6c31fb8
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-02 20:37:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from selenium import webdriver
 from features.login.login_whatsapp import WhatsAppLogin
-from features.send.send_message import send_text_message
+from features.send.send_message import send_message
 from features.send.send_image import send_image
 from features.send.send_video import send_video
 from features.receive.receive_message import receive_messages
@@ -10,14 +10,14 @@ from features.receive.receive_message import receive_messages
 def main():
     try:
         # Initialize the Chrome WebDriver (you may need to specify the driver path)
-        driver = webdriver.Chrome(executable_path="path/to/chromedriver.exe")
+        driver = webdriver.Chrome(executable_path="drivers/chromedriver.exe")
 
         # Initialize WhatsAppLogin and log in
         whatsapp_login = WhatsAppLogin(driver)
         whatsapp_login.login()
 
         # Example: Send a text message
-        send_text_message(driver, "Contact Name", "Hello, this is a test message!")
+        send_message(driver, "Contact Name", "Hello, this is a test message!")
 
         # Example: Send an image
         send_image(driver, "Contact Name", "path/to/image.jpg")
