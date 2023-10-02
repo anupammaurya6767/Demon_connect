a484c8571772adfdcb90f9d21051c8baf7722265
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-02 20:32:55 +0530

    Update send_image.py

--- features/send/send_image.py
+++ features/send/send_image.py
@@ -1,37 +1,34 @@
-# send_image.py
+# features/send/send_image.py
 
-import time
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from utils.helpers import find_element_with_retry
 
 def send_image(driver, contact_name, image_path):
-    """
-    Send an image to a contact or group on WhatsApp Web.
+    try:
+        # Locate the attachment button
+        attachment_button = driver.find_element_by_xpath(
+            '//*[@id="main"]/footer/div/div/span[1]/div[1]/div/span'
+        )
+        attachment_button.click()
 
-    :param driver: WebDriver instance
-    :param contact_name: Name of the contact or group
-    :param image_path: Path to the image file to send
-    :return: True if the image is sent successfully, False otherwise
-    """
-    # Find and click on the contact or group
-    contact_element = find_element_with_retry(driver, f"//span[@title='{contact_name}']")
-    if contact_element:
-        contact_element.click()
+        # Select the photo/video option
+        photo_video_option = driver.find_element_by_xpath(
+            '//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]'
+        )
+        photo_video_option.send_keys(image_path)
 
-        # Find the attachment button and click it to open the menu
-        attachment_button = find_element_with_retry(driver, "//div[@title='Attach']")
-        if attachment_button:
-            attachment_button.click()
+        # Wait for the image to be attached
+        driver.implicitly_wait(5)
 
-            # Find and click on the 'Photo & Video Library' option
-            photo_video_option = find_element_with_retry(driver, "//input[@accept='image/*,video/mp4,video/3gpp,video/quicktime']")
-            if photo_video_option:
-                photo_video_option.send_keys(image_path)
-                time.sleep(2)  # Wait for the image to upload
-                send_button = find_element_with_retry(driver, "//span[@data-icon='send']")
-                if send_button:
-                    send_button.click()
-                    return True
+        # Click the send button
+        send_button = driver.find_element_by_xpath(
+            '//*[@id="app"]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/span/div/div'
+        )
+        send_button.click()
 
-    return False
+        print(f"Image sent to {contact_name}: {image_path}")
+
+    except Exception as e:
+        print(f"Failed to send an image to {contact_name}: {str(e)}")
+
+# Usage Example:
+# send_image(driver, "Contact Name", "path/to/image.jpg")
