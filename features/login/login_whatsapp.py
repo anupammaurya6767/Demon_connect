df5b12c5e44798d6cca5fa48bf9b71836b3a8f5a
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-02 20:36:59 +0530

    Update login_whatsapp.py

--- features/login/login_whatsapp.py
+++ features/login/login_whatsapp.py
@@ -1,37 +1,24 @@
-# login_whatsapp.py
+# features/login/login_whatsapp.py
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
-class WhatsAppLogin:
-    def __init__(self, driver_path):
-        self.driver = webdriver.Chrome(executable_path=driver_path)
-
-    def wait_for_login(self):
+def login_whatsapp(driver):
+    try:
         # Open WhatsApp Web
-        self.driver.get("https://web.whatsapp.com/")
+        driver.get("https://web.whatsapp.com/")
 
-        # Wait for the user to scan the QR code manually for 20 seconds
-        input("Scan the QR code manually and press Enter after scanning...")
+        # Wait for the QR code to be scanned by the user
+        WebDriverWait(driver, 600).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div/div[2]/div/div[1]/div[2]/div/div[2]'))
+        )
+        
+        print("Successfully logged in to WhatsApp Web!")
 
-    def login(self):
-        # Check if already logged in
-        if "WhatsApp" in self.driver.title:
-            print("Already logged in.")
-        else:
-            self.wait_for_login()
-            print("Logged in successfully.")
+    except TimeoutException:
+        print("QR code scan timed out. Please try again.")
 
-    def logout(self):
-        # Close the browser
-        self.driver.quit()
-
-# Example usage
-if __name__ == "__main__":
-    # Provide the path to the Chrome WebDriver executable
-    chrome_driver_path = "path/to/chromedriver.exe"  # Update with the correct path
-
-    # Initialize WhatsAppLogin and log in
-    whatsapp_login = WhatsAppLogin(chrome_driver_path)
-    whatsapp_login.login()
+# Usage Example:
+# Call login_whatsapp(driver) function in your main.py script to log in to WhatsApp Web.
