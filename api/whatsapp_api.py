88eee2561e36df0a35e45f7b41af2ec266da7198
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-03 08:11:11 +0530

    Update whatsapp_api.py

--- api/whatsapp_api.py
+++ api/whatsapp_api.py
@@ -1,62 +1,49 @@
-# Import necessary libraries
+# api/whatsapp_api.py
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
-from utils.constants import WHATSAPP_WEB_URL, LOGIN_TIMEOUT_SECONDS
-import time
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from features.login.login_whatsapp import login_whatsapp
+from features.send.send_message import send_message
+from features.send.send_image import send_image
+from features.send.send_video import send_video
+from features.receive.receive_message import receive_message
+from features.send.send_group_message import send_group_message
+from features.delete.delete_message import delete_message
 
-# Define the WhatsApp API class
-class WhatsAppAPI:
+class Demon:
     def __init__(self):
-        # Initialize the WebDriver (e.g., Chrome)
-        self.driver = webdriver.Chrome()
-        self.driver.get(WHATSAPP_WEB_URL)
-        self.wait_for_login()
+        self.driver = self.initialize_driver()
 
-    def wait_for_login(self):
-        # Wait for the user to scan the QR code manually for 10 seconds
-        input("Scan the QR code manually and press Enter after scanning...")
+    def initialize_driver(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--user-data-dir=./User_Data")  # Modify this path as needed
+        return webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
-    def send_message(self, contact_name, message):
-        # Find the contact by name and send a message
-        contact_element = self.find_contact(contact_name)
-        if contact_element:
-            contact_element.click()
-            message_box = self.find_element_with_retry("//div[@class='_2S1VP copyable-text selectable-text']")
-            if message_box:
-                message_box.click()
-                message_box.send_keys(message)
-                message_box.send_keys(Keys.RETURN)
+    def login(self):
+        login_whatsapp(self.driver)
+
+    def send_message(self, message):
+        send_message(self.driver, message)
+
+    def send_image(self, image_path):
+        send_image(self.driver, image_path)
+
+    def send_video(self, video_path):
+        send_video(self.driver, video_path)
 
     def receive_message(self):
-        # Check for and retrieve incoming messages
-        pass  # Implement this method to receive and process messages
+        return receive_message(self.driver)
 
-    def find_contact(self, contact_name):
-        # Find and click on a contact by name
-        pass  # Implement this method to locate and click on a contact
+    def send_group_message(self, group_name, message):
+        send_group_message(self.driver, group_name, message)
 
-    def find_element_with_retry(self, xpath, max_attempts=3):
-        # Helper function to find an element with retries
-        pass  # Implement this method to find an element with retries
+    def delete_message(self, group_name, message):
+        delete_message(self.driver, group_name, message)
 
-    def logout(self):
-        # Log out of WhatsApp Web
+    def close(self):
         self.driver.quit()
 
-# Main execution
-if __name__ == "__main__":
-    # Initialize the WhatsApp API
-    whatsapp = WhatsAppAPI()
-
-    # Example: Send a message
-    whatsapp.send_message("contact_name", "Hello, this is a test message!")
-
-    # Example: Receive and process incoming messages
-    while True:
-        incoming_message = whatsapp.receive_message()
-        if incoming_message:
-            print(f"Received message: {incoming_message}")
-            # Process the message as needed
-
-    # To log out, call whatsapp.logout()
+# Usage Example:
+# You can create an instance of Demon and use its methods to interact with WhatsApp Web.
+# Make sure to call close() when you're done to close the browser.
