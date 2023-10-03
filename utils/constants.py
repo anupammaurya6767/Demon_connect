37f6e7d2cbdfe2eef8d15c26451fdc355dfbe2d1
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-03 18:34:47 +0530

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -3,6 +3,9 @@
 # URL for WhatsApp Web
 WHATSAPP_WEB_URL = "https://web.whatsapp.com/"
 
+BROWSER = chrome #firefox or 
+BROWSER_PATH = /home/user/.mozilla/firefox/xxxxxxxx.default or /home/user/.config/google-chrome/Default (this paths are for Linux)
+
 # File paths for WebDriver executables
 CHROME_DRIVER_PATH = "drivers/chromedriver.exe"
 GECKO_DRIVER_PATH = "drivers/geckodriver.exe"
